621d7d671a9432af721c2dcef03490fa95eef04a
Artemy <artemij87work@gmail.com>
2022-04-05 12:32:09 +0300

    Updates

---
+++ precache-manifest.80d7e1ca037c17b2458acd0138f349dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e4f768fd2084263f3b5fc4ab98f1ac",
+    "url": "/homeworks_by_ignat/index.html"
+  },
+  {
+    "revision": "2dcf3f07e127875b5fee",
+    "url": "/homeworks_by_ignat/static/css/main.0068b85e.chunk.css"
+  },
+  {
+    "revision": "037b259136890598b7ed",
+    "url": "/homeworks_by_ignat/static/js/2.402fe758.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homeworks_by_ignat/static/js/2.402fe758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dcf3f07e127875b5fee",
+    "url": "/homeworks_by_ignat/static/js/main.1de6bcd6.chunk.js"
+  },
+  {
+    "revision": "165c05c3b4cc28ccaa3a",
+    "url": "/homeworks_by_ignat/static/js/runtime-main.239eb6b4.js"
+  }
+]);
